a1f8e632a458e556cdaf8cb525d3909e8b4799bb
Wooangha <85687189+Wooangha@users.noreply.github.com>
2025-06-11 14:24:38 +0900

    [Fix] seven seg decoder fixed

--- code/s_seg.v
+++ code/s_seg.v
@@ -11,12 +11,12 @@ module bcd_to_7seg(
     assign y = bcd_in[1];
     assign z = bcd_in[0];
 
-    assign seg7_out[0] = (~x & ~z) | (w & ~z) | (x & y) | (~w & y) | (~w & x & z) | (w & ~x & ~y); // a
-    assign seg7_out[1] = (~x & ~z) | (~w & ~x) | (w & ~y & z) | (~w & ~y & ~z) | (~w & y & z); // b
-    assign seg7_out[2] = (~w & x) | (w & ~x) | (~y & z) | (~x & ~y) | (~x & z); //c
-    assign seg7_out[3] = (w & ~y) | (~w & ~x & ~z) | (~x & y & z) | (x & ~y & z) | (x & y & ~z); // d
-    assign seg7_out[4] = (~x & ~z) | (w & x) | (y & ~z) | (w & y); // e
-    assign seg7_out[5] = (~y & ~z) | (w & ~x) | (x & ~z) | (w & y) | (~w & x & y); // f
-    assign seg7_out[6] = (w & ~x) | (y & ~z) | (w & z) | (~x & y) | (~w & x & ~y); // g
+    assign seg7_out[0] = ~((~x & ~z) | (w & ~z) | (x & y) | (~w & y) | (~w & x & z) | (w & ~x & ~y)); // a
+    assign seg7_out[1] = ~((~x & ~z) | (~w & ~x) | (w & ~y & z) | (~w & ~y & ~z) | (~w & y & z)); // b
+    assign seg7_out[2] = ~((~w & x) | (w & ~x) | (~y & z) | (~x & ~y) | (~x & z)); // c
+    assign seg7_out[3] = ~((w & ~y) | (~w & ~x & ~z) | (~x & y & z) | (x & ~y & z) | (x & y & ~z)); // d
+    assign seg7_out[4] = ~((~x & ~z) | (w & x) | (y & ~z) | (w & y)); // e
+    assign seg7_out[5] = ~((~y & ~z) | (w & ~x) | (x & ~z) | (w & y) | (~w & x & ~y)); // f
+    assign seg7_out[6] = ~((w & ~x) | (y & ~z) | (w & z) | (~x & y) | (~w & x & ~y)); // g
 
 endmodule
